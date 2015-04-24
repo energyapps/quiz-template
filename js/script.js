5c88b3f877736001908d634fd5cc3bf213d30941
Daniel Wood <danielwood@Daniels-MacBook-Pro.local>
2015-04-24 15:01:18 -0400

    have even answers, taken from grid quiz

--- js/script.js
+++ js/script.js
@@ -10,22 +10,55 @@ function shuffle(o){ //v1.0
 	return o;
 };
 
+// define how many characters = a line break!!! so that side by side columns have the same size
+var size = [30,65,95,125];
+
 // loop through the questions
 for (var i = 0; i < data.length; i++) {
 
 	var raw = [
-		{'ans':data[i].ans1,'correct':'correct'},{'ans':data[i].ans2,'correct':''},{'ans':data[i].ans3,'correct':''},{'ans':data[i].ans4,'correct':''}
+		{'ans':data[i].ans1,'correct':'correct','lgth':data[i].ans1.length},
+		{'ans':data[i].ans2,'correct':'','lgth':data[i].ans2.length},
+		{'ans':data[i].ans3,'correct':'','lgth':data[i].ans3.length},
+		{'ans':data[i].ans4,'correct':'','lgth':data[i].ans4.length}
 	];
 	
 	// scramble the answers
 	shuffle(raw)
+
+// Add in classes to define the heights of the questions, one to its random neighbor.
+	if (raw[0].lgth > size[3] || raw[1].lgth > size[3]) {
+		var hgtclass1 = "xxlarge-a";	
+	} else if (raw[0].lgth > size[2] || raw[1].lgth > size[2]) {
+		var hgtclass1 = "xlarge-a";
+	} else if (raw[0].lgth > size[1] || raw[1].lgth > size[1]) {
+		var hgtclass1 = "large-a";
+	} else if (raw[0].lgth > size[0] || raw[1].lgth > size[0]) { 
+		var hgtclass1 = "medium-a";
+	} else {
+		var hgtclass1 = "small-a";
+	}
+
+	if (raw[2].lgth > size[3] || raw[3].lgth > size[3]) {
+		var hgtclass2 = "xxlarge-a";	
+	} else if (raw[2].lgth > size[2] || raw[3].lgth > size[2]) {
+		var hgtclass2 = "xlarge-a";
+	} else if (raw[2].lgth > size[1] || raw[3].lgth > size[1]) {
+		var hgtclass2 = "large-a";
+	} else if (raw[2].lgth > size[0] || raw[3].lgth > size[0]) { 
+		var hgtclass2 = "medium-a";
+	} else {
+		var hgtclass2 = "small-a";
+	}
 
 	// set content to have answers in them		
 		// header image conditional
 		if (data[i].img === "") {
 			var headImg =  "";
 		} else {
+			// var headImg =  '<img src="http://energy.gov/sites/prod/files/' + data[i].img + '.jpg">'		
 			var headImg =  '<img src="' + data[i].img + '">'		
+			// var headImg =  '<img src="http:energy.gov/sites/prod/files/' + data[i].img + '.jpg">'		
 		};
 
 		// context info conditional
@@ -43,13 +76,13 @@ for (var i = 0; i < data.length; i++) {
 	data[i].question +
 	'</p></div><div class="rowDOE full-size-blocks"><div class="large-12 columnsDOE map-image">' +
 	headImg +
-	'</div></div><div class="answers"><div class="large-6 columnsDOE halves"><div class="medium-6 small-12 columnsDOE a-options first-c"><div class="large-12 medium-12 small-12 small-centered a-bg q' + (i+1) + ' ' + raw[0].correct + '" data-id=' + i + '><p>' +
+	'</div></div><div class="answers"><div class="large-12 columnsDOE halves"><div class="medium-6 small-12 columnsDOE a-options first-c"><div class="large-12 medium-12 small-12 small-centered a-bg ' + hgtclass1 + ' q' + (i+1) + ' ' + raw[0].correct + '" data-id=' + i + '><p>' +
 	raw[0].ans + 
-	'</p></div></div><div class="medium-6 small-12 columnsDOE a-options"><div class="large-12 medium-12 small-12 small-centered a-bg q' + (i+1) + ' ' + raw[1].correct + '" data-id=' + i + '><p>' +
+	'</p></div></div><div class="medium-6 small-12 columnsDOE a-options"><div class="large-12 medium-12 small-12 small-centered a-bg ' + hgtclass1 + ' q' + (i+1) + ' ' + raw[1].correct + '" data-id=' + i + '><p>' +
 	raw[1].ans +
-	'</p></div></div></div><div class="large-6 columnsDOE halves"><div class="medium-6 small-12 columnsDOE a-options"><div class="large-12 medium-12 small-12 small-centered a-bg q' + (i+1) + ' ' + raw[2].correct + '" data-id=' + i + '><p>' +
+	'</p></div></div></div><div class="large-12 columnsDOE halves"><div class="medium-6 small-12 columnsDOE a-options"><div class="large-12 medium-12 small-12 small-centered a-bg ' + hgtclass2 + ' q' + (i+1) + ' ' + raw[2].correct + '" data-id=' + i + '><p>' +
 	raw[2].ans +
-	'</p></div></div><div class="medium-6 small-12 columnsDOE a-options last-c"><div class="large-12 medium-12 small-12 small-centered a-bg q' + (i+1) + ' ' + raw[3].correct + '" data-id=' + i + '><p>' +
+	'</p></div></div><div class="medium-6 small-12 columnsDOE a-options last-c"><div class="large-12 medium-12 small-12 small-centered a-bg ' + hgtclass2 + ' q' + (i+1) + ' ' + raw[3].correct + '" data-id=' + i + '><p>' +
 	raw[3].ans +
 	'</p></div></div></div></div>' +
 	contInfo +
@@ -72,6 +105,7 @@ for (var k = 0; k < NumOfQuestions; k++) {
 	QuestionIndex.push(0);
 };
 
+(function ($) { 
 //clicking the first time per question causes a question to be answered. after that it does nothing. (see if statement inside)
 $('.a-bg').click(function (e) {
 	e.preventDefault();
@@ -117,7 +151,7 @@ $('.a-bg').click(function (e) {
 		};
 	};
 });
-
+}(jQuery));  
 
 
 // on each click of button, change total correct/incorrect number....use that as a trigger
