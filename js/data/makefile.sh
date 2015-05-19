413cd43a01378931001afe7f8d56822ac57419bd
Daniel Wood <danielwood@Daniels-MacBook-Pro.local>
2015-05-19 08:27:30 -0400

    makefiles and python

---
+++ js/data/makefile.sh
@@ -0,0 +1,7 @@
+#Install csvkit stuff
+./installation.sh
+
+#Run csvkit to get the needed json out of csv on the other end. 
+
+
+#Run some script that places the new json in the script and reruns the JAUNT!
---
+++ js/data/test.sh
@@ -0,0 +1,13 @@
+#check to see if installed 
+
+VALID_PASSWORD="secret" #this is our password.
+
+echo "Please enter the password:"
+read PASSWORD
+echo $PASSWORD
+
+if [ "$PASSWORD" == "$VALID_PASSWORD" ]; then
+	echo "You have access!"
+else
+	echo "ACCESS DENIED!"
+fi
